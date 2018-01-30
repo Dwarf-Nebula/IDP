aee4db5f6ff22d0129224159f06588ccb249a505
Dwarf-Nebula <Dwarf-Nebula@users.noreply.github.com>
2018-01-30 16:12:53 +0100

    Changed from GPIO.BOARD to GPIO.BCM

--- hardware/ledring.py
+++ hardware/ledring.py
@@ -4,7 +4,7 @@ from neopixel import *
 # LED configuration:
 LED_COUNT_SMALL = 24      # Number of LED pixels.
 LED_COUNT_BIG   = 22      # Number of LED pixels.
-LED_PIN         = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_PIN         = 12      # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ     = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA         = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS  = 255     # Set to 0 for darkest and 255 for brightest
--- hardware/loopband.py
+++ hardware/loopband.py
@@ -13,12 +13,12 @@ bezig = 0
 snelheid = 0
 url = "http://benno.using.ovh/request.php"
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(29, GPIO.IN) # set pin 29 as an input
-GPIO.setup(31, GPIO.IN) # set pin 31 as an input
-GPIO.setup(37, GPIO.IN) # set pin 37 as an input
-GPIO.setup(33, GPIO.OUT)    # set GPIO 33 as output for the PWM signal
-motor = GPIO.PWM(33, 500)    # create object motor for PWM on port 33 at 1KHz
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(5, GPIO.IN) # set pin 29 as an input
+GPIO.setup(6, GPIO.IN) # set pin 31 as an input
+GPIO.setup(26, GPIO.IN) # set pin 37 as an input
+GPIO.setup(13, GPIO.OUT)    # set GPIO 33 as output for the PWM signal
+motor = GPIO.PWM(13, 500)    # create object motor for PWM on port 33 at 1KHz
 motor.start(0) # start the pwm, but off
 
 try:
@@ -29,15 +29,12 @@ try:
         r = requests.post(url, data=payload)
         print(r.text)"""
         openin(ring_big)
-        #GPIO.output(33, GPIO.HIGH)
         snelheid = 25
         motor.ChangeDutyCycle(snelheid)
         drawspeed(snelheid)
-        #time.sleep(5)
         while True:
             hoger_state = GPIO.input(29)
             time.sleep(0.01)
-            #print(hoger_state)
             if (hoger_state == True and snelheid < 100):
                 time.sleep(0.1)
                 print("omhoog")
@@ -48,7 +45,6 @@ try:
             
             lager_state = GPIO.input(31)
             time.sleep(0.01)
-            #print(lager_state)
             if (lager_state == True and snelheid > 0):
                 time.sleep(0.1)
                 print("omlaag")
@@ -59,7 +55,6 @@ try:
             
             card_state = GPIO.input(37)
             time.sleep(0.01)
-            #print(card_state)
             if (card_state == True):
                 time.sleep(0.1)
                 print("card")
@@ -70,8 +65,7 @@ try:
                     break
                 else:
                     continue
-            
-        #GPIO.output(33, GPIO.LOW)
+        
         snelheid = 0
         motor.ChangeDutyCycle(snelheid)
         drawspeed(snelheid)
