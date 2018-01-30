065b506a5a9514e54bf77965a6333e129641f9de
Dwarf-Nebula <Dwarf-Nebula@users.noreply.github.com>
2018-01-30 11:02:49 +0100

    Fixed name error

--- hardware/ledring.py
+++ hardware/ledring.py
@@ -13,9 +13,9 @@ LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 
 # Create NeoPixel object with appropriate configuration.
-strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+ring = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
 # Intialize the library (must be called once before other functions).
-strip.begin()
+ring.begin()
 
 def wait(ms):
     time.sleep(ms/1000)
