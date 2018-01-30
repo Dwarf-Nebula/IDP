457243eae48557c07a837e3a67c339290bd9abc2
Dwarf-Nebula <Dwarf-Nebula@users.noreply.github.com>
2018-01-30 15:39:24 +0100

    Added display

---
+++ hardware/display.py
@@ -0,0 +1,27 @@
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=24)
+
+disp.begin()
+disp.clear()
+disp.display()
+
+# Get display width and height.
+width = disp.width
+height = disp.height
+
+image = Image.new('1', (width, height))
+
+draw = ImageDraw.Draw(image)
+draw.rectangle((0,0,width-1,height-1), outline=1, fill=0)
+
+font = ImageFont.load_default()
+draw.text((16, 8),'RASPBERRYTIPS.NL',  font=font, fill=255)
+
+disp.image(image)
+disp.display()
